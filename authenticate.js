6fe08b10b9645d13a6c1f920243fbdf60f9b2a8b
Sabby <sabby_india@hotmail.com>
2019-02-25 10:36:46 -0600

    Passport Facebook OAuth2 Implementation

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ var User = require('./models/user');
 var JwtStrategy = require('passport-jwt').Strategy;
 var ExtractJwt = require('passport-jwt').ExtractJwt;
 var jwt = require('jsonwebtoken'); //used to create, sign, verify tokens
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 var config = require('./config');
 
@@ -48,3 +49,35 @@ exports.jwtPassport = passport.use(new JwtStrategy(opts,
             next(err);
         }
     };
+
+//facebook strategy
+    exports.FacebookPassport = passport.use(new 
+        FacebookTokenStrategy({
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+            },
+            (accessToken, refreshToken, profile, done) =>{//getting the facebookId
+                User.findOne({facebookId: profile.id},(err, user) => 
+                {
+                    if(err){
+                        return done( err, false);
+                    }    
+                    if(!err && user !== null){ //if user exists already
+                        return done(null, user);
+                    }
+                    else{ //if user not exist then create a new user
+                        user = new User({username: profile.displayName});
+                        user.facebookId = profile.id;
+                        user.firstname = profile.name.givenName;
+                        user.lastname = profile.name.familyName;
+                        user.save((err, user) => {
+                            if(err)
+                                return done(err, false);
+                            else
+                                return done(null, user);
+                        })
+                    }
+                }); 
+               
+            }
+    ));
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey' : '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook' : {
+        clientId: '791461164550020',
+        clientSecret: '9ca0a07c67fce3b475082d80975fe1e6'
+    }
 }
